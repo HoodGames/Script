2eb734113ef26c13680d9e49900259e5fb791475
HoodGames <122266617+HoodGames@users.noreply.github.com>
2025-04-06 05:57:30 +0800

    Updated LockNgPacker.lua

--- LockNgPacker.lua
+++ LockNgPacker.lua
@@ -4,24 +4,19 @@ local Vector2new, Vector3zero, CFramenew, Color3fromRGB, Color3fromHSV, Drawingn
 local getupvalue, mousemoverel, tablefind, tableremove, stringlower, stringsub, mathclamp = debug.getupvalue, mousemoverel or (Input and Input.MouseMove), table.find, table.remove, string.lower, string.sub, math.clamp
 
 local GameMetatable = getrawmetatable and getrawmetatable(game) or {
-    __index = function(self, Index)
-        return self[Index]
-    end,
-    __newindex = function(self, Index, Value)
-        self[Index] = Value
-    end
+	__index = function(self, Index) return self[Index] end,
+	__newindex = function(self, Index, Value) self[Index] = Value end
 }
 
 local __index = GameMetatable.__index
 local __newindex = GameMetatable.__newindex
+
 local getrenderproperty, setrenderproperty = getrenderproperty or __index, setrenderproperty or __newindex
 local GetService = __index(game, "GetService")
-
 local RunService = GetService(game, "RunService")
 local UserInputService = GetService(game, "UserInputService")
 local TweenService = GetService(game, "TweenService")
 local Players = GetService(game, "Players")
-
 local LocalPlayer = __index(Players, "LocalPlayer")
 local Camera = __index(workspace, "CurrentCamera")
 
@@ -36,46 +31,49 @@ local RequiredDistance, Typing, Running, ServiceConnections, Animation, Original
 local Connect, Disconnect = __index(game, "DescendantAdded").Connect
 
 if ExunysDeveloperAimbot and ExunysDeveloperAimbot.Exit then
-    ExunysDeveloperAimbot:Exit()
+	ExunysDeveloperAimbot:Exit()
 end
 
 getgenv().ExunysDeveloperAimbot = {
-    DeveloperSettings = {
-        UpdateMode = "RenderStepped",
-        TeamCheckOption = "TeamColor",
-        RainbowSpeed = 1
-    },
-    Settings = {
-        Enabled = true,
-        TeamCheck = false,
-        AliveCheck = true,
-        WallCheck = false,
-        OffsetToMoveDirection = false,
-        OffsetIncrement = 15,
-        Sensitivity = 0,
-        Sensitivity2 = 3.5,
-        LockMode = 1,
-        LockPart = "Head",
-        TriggerKey = Enum.UserInputType.MouseButton2,
-        Toggle = false
-    },
-    FOVSettings = {
-        Enabled = true,
-        Visible = false,
-        Radius = 90,
-        NumSides = 60,
-        Thickness = 1,
-        Transparency = 1,
-        Filled = false,
-        RainbowColor = false,
-        RainbowOutlineColor = false,
-        Color = Color3fromRGB(255, 255, 255),
-        OutlineColor = Color3fromRGB(0, 0, 0),
-        LockedColor = Color3fromRGB(255, 150, 150)
-    },
-    Blacklisted = {},
-    FOVCircleOutline = Drawingnew("Circle"),
-    FOVCircle = Drawingnew("Circle")
+	DeveloperSettings = {
+		UpdateMode = "RenderStepped",
+		TeamCheckOption = "TeamColor",
+		RainbowSpeed = 1
+	},
+
+	Settings = {
+		Enabled = true,
+		TeamCheck = false,
+		AliveCheck = true,
+		WallCheck = false,
+		OffsetToMoveDirection = false,
+		OffsetIncrement = 15,
+		Sensitivity = 0,
+		Sensitivity2 = 3.5,
+		LockMode = 1,
+		LockPart = "Head",
+		TriggerKey = Enum.UserInputType.MouseButton2,
+		Toggle = false
+	},
+
+	FOVSettings = {
+		Enabled = true,
+		Visible = false,
+		Radius = 90,
+		NumSides = 60,
+		Thickness = 1,
+		Transparency = 1,
+		Filled = false,
+		RainbowColor = false,
+		RainbowOutlineColor = false,
+		Color = Color3fromRGB(255, 255, 255),
+		OutlineColor = Color3fromRGB(0, 0, 0),
+		LockedColor = Color3fromRGB(255, 150, 150)
+	},
+
+	Blacklisted = {},
+	FOVCircleOutline = Drawingnew("Circle"),
+	FOVCircle = Drawingnew("Circle")
 }
 
 local Environment = getgenv().ExunysDeveloperAimbot
@@ -83,160 +81,171 @@ setrenderproperty(Environment.FOVCircle, "Visible", false)
 setrenderproperty(Environment.FOVCircleOutline, "Visible", false)
 
 local FixUsername = function(String)
-    local Result
-    for _, Value in next, GetPlayers(Players) do
-        local Name = __index(Value, "Name")
-        if stringsub(stringlower(Name), 1, #String) == stringlower(String) then
-            Result = Name
-        end
-    end
-    return Result
+	local Result
+	for _, Value in next, GetPlayers(Players) do
+		local Name = __index(Value, "Name")
+		if stringsub(stringlower(Name), 1, #String) == stringlower(String) then
+			Result = Name
+		end
+	end
+	return Result
+end
+
+local GetRainbowColor = function()
+	local RainbowSpeed = Environment.DeveloperSettings.RainbowSpeed
+	return Color3fromHSV(tick() % RainbowSpeed / RainbowSpeed, 1, 1)
 end
 
 local ConvertVector = function(Vector)
-    return Vector2new(Vector.X, Vector.Y)
+	return Vector2new(Vector.X, Vector.Y)
 end
 
 local CancelLock = function()
-    Environment.Locked = nil
-    local FOVCircle = Environment.FOVCircle
-    setrenderproperty(FOVCircle, "Color", Environment.FOVSettings.Color)
-    __newindex(UserInputService, "MouseDeltaSensitivity", OriginalSensitivity)
-    if Animation then
-        Animation:Cancel()
-    end
+	Environment.Locked = nil
+	local FOVCircle = Environment.FOVCircle
+	setrenderproperty(FOVCircle, "Color", Environment.FOVSettings.Color)
+	__newindex(UserInputService, "MouseDeltaSensitivity", OriginalSensitivity)
+	if Animation then Animation:Cancel() end
 end
 
 local GetClosestPlayer = function()
-    local Settings = Environment.Settings
-    local LockPart = Settings.LockPart
-    if not Environment.Locked then
-        RequiredDistance = 2000
-        for _, Value in next, GetPlayers(Players) do
-            local Character = __index(Value, "Character")
-            local Humanoid = Character and FindFirstChildOfClass(Character, "Humanoid")
-            if Value ~= LocalPlayer and not tablefind(Environment.Blacklisted, __index(Value, "Name")) and Character and FindFirstChild(Character, LockPart) and Humanoid then
-                local PartPosition, TeamCheckOption = __index(Character[LockPart], "Position"), Environment.DeveloperSettings.TeamCheckOption
-                if Settings.TeamCheck and __index(Value, TeamCheckOption) == __index(LocalPlayer, TeamCheckOption) then continue end
-                if Settings.AliveCheck and __index(Humanoid, "Health") <= 0 then continue end
-                if Settings.WallCheck then
-                    local BlacklistTable = GetDescendants(__index(LocalPlayer, "Character"))
-                    for _, Value in next, GetDescendants(Character) do
-                        BlacklistTable[#BlacklistTable + 1] = Value
-                    end
-                    if #GetPartsObscuringTarget(Camera, {PartPosition}, BlacklistTable) > 0 then continue end
-                end
-                local Vector, OnScreen, Distance = WorldToViewportPoint(Camera, PartPosition)
-                Vector = ConvertVector(Vector)
-                Distance = (GetMouseLocation(UserInputService) - Vector).Magnitude
-                if Distance < RequiredDistance and OnScreen then
-                    RequiredDistance, Environment.Locked = Distance, Value
-                end
-            end
-        end
-    elseif (GetMouseLocation(UserInputService) - ConvertVector(WorldToViewportPoint(Camera, __index(__index(__index(Environment.Locked, "Character"), LockPart), "Position")))).Magnitude > RequiredDistance then
-        CancelLock()
-    end
+	local Settings = Environment.Settings
+	local LockPart = Settings.LockPart
+	if not Environment.Locked then
+		RequiredDistance = Environment.FOVSettings.Enabled and Environment.FOVSettings.Radius or 2000
+		for _, Value in next, GetPlayers(Players) do
+			local Character = __index(Value, "Character")
+			local Humanoid = Character and FindFirstChildOfClass(Character, "Humanoid")
+			if Value ~= LocalPlayer and not tablefind(Environment.Blacklisted, __index(Value, "Name")) and Character and FindFirstChild(Character, LockPart) and Humanoid then
+				local PartPosition = __index(Character[LockPart], "Position")
+				local TeamCheckOption = Environment.DeveloperSettings.TeamCheckOption
+				if Settings.TeamCheck and __index(Value, TeamCheckOption) == __index(LocalPlayer, TeamCheckOption) then continue end
+				if Settings.AliveCheck and __index(Humanoid, "Health") <= 0 then continue end
+				if Settings.WallCheck then
+					local BlacklistTable = GetDescendants(__index(LocalPlayer, "Character"))
+					for _, Value in next, GetDescendants(Character) do
+						BlacklistTable[#BlacklistTable + 1] = Value
+					end
+					if #GetPartsObscuringTarget(Camera, {PartPosition}, BlacklistTable) > 0 then continue end
+				end
+				local Vector, OnScreen, Distance = WorldToViewportPoint(Camera, PartPosition)
+				Vector = ConvertVector(Vector)
+				Distance = (GetMouseLocation(UserInputService) - Vector).Magnitude
+				if Distance < RequiredDistance and OnScreen then
+					RequiredDistance, Environment.Locked = Distance, Value
+				end
+			end
+		end
+	elseif (GetMouseLocation(UserInputService) - ConvertVector(WorldToViewportPoint(Camera, __index(__index(__index(Environment.Locked, "Character"), LockPart), "Position")))).Magnitude > RequiredDistance then
+		CancelLock()
+	end
 end
 
 local Load = function()
-    OriginalSensitivity = __index(UserInputService, "MouseDeltaSensitivity")
-    local Settings, FOVCircle, FOVCircleOutline, Offset = Environment.Settings, Environment.FOVCircle, Environment.FOVCircleOutline
-    ServiceConnections.RenderSteppedConnection = Connect(__index(RunService, Environment.DeveloperSettings.UpdateMode), function()
-        local OffsetToMoveDirection, LockPart = Settings.OffsetToMoveDirection, Settings.LockPart
-        if Running and Settings.Enabled then
-            GetClosestPlayer()
-            Offset = OffsetToMoveDirection and __index(FindFirstChildOfClass(__index(Environment.Locked, "Character"), "Humanoid"), "MoveDirection") * (mathclamp(Settings.OffsetIncrement, 1, 30) / 10) or Vector3zero
-            if Environment.Locked then
-                local LockedPosition_Vector3 = __index(__index(Environment.Locked, "Character")[LockPart], "Position")
-                local LockedPosition = WorldToViewportPoint(Camera, LockedPosition_Vector3 + Offset)
-                if Environment.Settings.LockMode == 2 then
-                    mousemoverel((LockedPosition.X - GetMouseLocation(UserInputService).X) / Settings.Sensitivity2, (LockedPosition.Y - GetMouseLocation(UserInputService).Y) / Settings.Sensitivity2)
-                else
-                    if Settings.Sensitivity > 0 then
-                        Animation = TweenService:Create(Camera, TweenInfonew(Environment.Settings.Sensitivity, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CFrame = CFramenew(Camera.CFrame.Position, LockedPosition_Vector3)})
-                        Animation:Play()
-                    else
-                        __newindex(Camera, "CFrame", CFramenew(Camera.CFrame.Position, LockedPosition_Vector3 + Offset))
-                    end
-                    __newindex(UserInputService, "MouseDeltaSensitivity", 0)
-                end
-            end
-        end
-    end)
+	OriginalSensitivity = __index(UserInputService, "MouseDeltaSensitivity")
+	local Settings, FOVCircle, FOVCircleOutline, FOVSettings = Environment.Settings, Environment.FOVCircle, Environment.FOVCircleOutline, Environment.FOVSettings
+	ServiceConnections.RenderSteppedConnection = Connect(__index(RunService, Environment.DeveloperSettings.UpdateMode), function()
+		local OffsetToMoveDirection, LockPart = Settings.OffsetToMoveDirection, Settings.LockPart
+		if FOVSettings.Enabled and Settings.Enabled then
+			for Index, Value in next, FOVSettings do
+				if Index == "Color" then continue end
+				if pcall(getrenderproperty, FOVCircle, Index) then
+					setrenderproperty(FOVCircle, Index, Value)
+					setrenderproperty(FOVCircleOutline, Index, Value)
+				end
+			end
+			setrenderproperty(FOVCircle, "Color", (Environment.Locked and FOVSettings.LockedColor) or FOVSettings.RainbowColor and GetRainbowColor() or FOVSettings.Color)
+			setrenderproperty(FOVCircleOutline, "Color", FOVSettings.RainbowOutlineColor and GetRainbowColor() or FOVSettings.OutlineColor)
+			setrenderproperty(FOVCircleOutline, "Thickness", FOVSettings.Thickness + 1)
+			setrenderproperty(FOVCircle, "Position", GetMouseLocation(UserInputService))
+			setrenderproperty(FOVCircleOutline, "Position", GetMouseLocation(UserInputService))
+		else
+			setrenderproperty(FOVCircle, "Visible", false)
+			setrenderproperty(FOVCircleOutline, "Visible", false)
+		end
+		if Running and Settings.Enabled then
+			GetClosestPlayer()
+			local Offset = OffsetToMoveDirection and __index(FindFirstChildOfClass(__index(Environment.Locked, "Character"), "Humanoid"), "MoveDirection") * (mathclamp(Settings.OffsetIncrement, 1, 30) / 10) or Vector3zero
+			if Environment.Locked then
+				local LockedPosition_Vector3 = __index(__index(Environment.Locked, "Character")[LockPart], "Position")
+				local LockedPosition = WorldToViewportPoint(Camera, LockedPosition_Vector3 + Offset)
+				if Environment.Settings.LockMode == 2 then
+					mousemoverel((LockedPosition.X - GetMouseLocation(UserInputService).X) / Settings.Sensitivity2, (LockedPosition.Y - GetMouseLocation(UserInputService).Y) / Settings.Sensitivity2)
+				else
+					if Settings.Sensitivity > 0 then
+						Animation = TweenService:Create(Camera, TweenInfonew(Settings.Sensitivity, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CFrame = CFramenew(Camera.CFrame.Position, LockedPosition_Vector3)})
+						Animation:Play()
+					else
+						__newindex(Camera, "CFrame", CFramenew(Camera.CFrame.Position, LockedPosition_Vector3 + Offset))
+					end
+					__newindex(UserInputService, "MouseDeltaSensitivity", 0)
+				end
+				setrenderproperty(FOVCircle, "Color", FOVSettings.LockedColor)
+			end
+		end
+	end)
 
-    ServiceConnections.InputBeganConnection = Connect(__index(UserInputService, "InputBegan"), function(Input)
-        local TriggerKey, Toggle = Settings.TriggerKey, Settings.Toggle
-        if Typing then return end
-        if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode == TriggerKey or Input.UserInputType == TriggerKey then
-            if Toggle then
-                Running = not Running
-                if not Running then CancelLock() end
-            else
-                Running = true
-            end
-        end
-    end)
+	ServiceConnections.InputBeganConnection = Connect(__index(UserInputService, "InputBegan"), function(Input)
+		local TriggerKey, Toggle = Settings.TriggerKey, Settings.Toggle
+		if Typing then return end
+		if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode == TriggerKey or Input.UserInputType == TriggerKey then
+			if Toggle then
+				Running = not Running
+				if not Running then CancelLock() end
+			else
+				Running = true
+			end
+		end
+	end)
 
-    ServiceConnections.InputEndedConnection = Connect(__index(UserInputService, "InputEnded"), function(Input)
-        local TriggerKey, Toggle = Settings.TriggerKey, Settings.Toggle
-        if Toggle or Typing then return end
-        if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode == TriggerKey or Input.UserInputType == TriggerKey then
-            Running = false
-            CancelLock()
-        end
-    end)
+	ServiceConnections.InputEndedConnection = Connect(__index(UserInputService, "InputEnded"), function(Input)
+		local TriggerKey, Toggle = Settings.TriggerKey, Settings.Toggle
+		if Toggle or Typing then return end
+		if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode == TriggerKey or Input.UserInputType == TriggerKey then
+			Running = false
+			CancelLock()
+		end
+	end)
 end
 
-ServiceConnections.TypingStartedConnection = Connect(__index(UserInputService, "TextBoxFocused"), function()
-    Typing = true
-end)
-
-ServiceConnections.TypingEndedConnection = Connect(__index(UserInputService, "TextBoxFocusReleased"), function()
-    Typing = false
-end)
+ServiceConnections.TypingStartedConnection = Connect(__index(UserInputService, "TextBoxFocused"), function() Typing = true end)
+ServiceConnections.TypingEndedConnection = Connect(__index(UserInputService, "TextBoxFocusReleased"), function() Typing = false end)
 
 function Environment.Exit(self)
-    assert(self, "Exit: Missing self")
-    for Index, _ in next, ServiceConnections do
-        Disconnect(ServiceConnections[Index])
-    end
-    Load = nil; ConvertVector = nil; CancelLock = nil; GetClosestPlayer = nil; FixUsername = nil
-    self.FOVCircle:Remove()
-    self.FOVCircleOutline:Remove()
-    getgenv().ExunysDeveloperAimbot = nil
+	assert(self, "Missing parameter #1 \"self\".")
+	for Index, _ in next, ServiceConnections do Disconnect(ServiceConnections[Index]) end
+	Load = nil; ConvertVector = nil; CancelLock = nil; GetClosestPlayer = nil; GetRainbowColor = nil; FixUsername = nil
+	self.FOVCircle:Remove()
+	self.FOVCircleOutline:Remove()
+	getgenv().ExunysDeveloperAimbot = nil
 end
 
 function Environment.Restart()
-    for Index, _ in next, ServiceConnections do
-        Disconnect(ServiceConnections[Index])
-    end
-    Load()
+	for Index, _ in next, ServiceConnections do Disconnect(ServiceConnections[Index]) end
+	Load()
 end
 
 function Environment.Blacklist(self, Username)
-    assert(self, "Blacklist: Missing self")
-    assert(Username, "Blacklist: Missing Username")
-    Username = FixUsername(Username)
-    assert(self, "Blacklist: User not found")
-    self.Blacklisted[#self.Blacklisted + 1] = Username
+	assert(self and Username, "Missing parameters.")
+	Username = FixUsername(Username)
+	assert(self, "User "..Username.." couldn't be found.")
+	self.Blacklisted[#self.Blacklisted + 1] = Username
 end
 
 function Environment.Whitelist(self, Username)
-    assert(self, "Whitelist: Missing self")
-    assert(Username, "Whitelist: Missing Username")
-    Username = FixUsername(Username)
-    assert(Username, "Whitelist: User not found")
-    local Index = tablefind(self.Blacklisted, Username)
-    assert(Index, "Whitelist: User is not blacklisted")
-    tableremove(self.Blacklisted, Index)
+	assert(self and Username, "Missing parameters.")
+	Username = FixUsername(Username)
+	assert(Username, "User "..Username.." couldn't be found.")
+	local Index = tablefind(self.Blacklisted, Username)
+	assert(Index, "User "..Username.." is not blacklisted.")
+	tableremove(self.Blacklisted, Index)
 end
 
 function Environment.GetClosestPlayer()
-    GetClosestPlayer()
-    local Value = Environment.Locked
-    CancelLock()
-    return Value
+	GetClosestPlayer()
+	local Value = Environment.Locked
+	CancelLock()
+	return Value
 end
 
 Environment.Load = Load
